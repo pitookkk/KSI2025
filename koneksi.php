2d173529e84c0481d28837785b36b5e52544a028
pitookkk <vitoaditya149@gmail.com>
2025-11-04 14:48:44 +0700

    Menambahkan file koneksi.php ke branch KoneksiDatabase

---
+++ koneksi.php
@@ -0,0 +1,16 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "db_mahasiswa";
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+
+if ($conn->connect_error) {
+    die("Koneksi Gagal: " . $conn->connect_error);
+}
+
+?>
